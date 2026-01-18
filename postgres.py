d7fd36b00d3e5fbf240d334910c6ce1ea5c835c7
fabianovich <fabianvanacoley@gmail.com>
2026-01-18 21:20:11 +0100

    new_user function

---
+++ postgres.py
@@ -0,0 +1,33 @@
+import psycopg2
+
+import credentials
+
+db_connection = psycopg2.connect(
+    database=credentials.database,
+    host=credentials.host,
+    user=credentials.user,
+    password=credentials.password,
+    port=credentials.port,
+)
+
+cursor = db_connection.cursor()
+
+# begin functions
+
+
+def new_user(name):
+    id = get_new_id()
+    cursor.execute(f"insert into people values ({id}, '{name}')")
+    db_connection.commit()
+
+
+def get_new_id():
+    cursor.execute("select max(id) from people")
+    current_highest_id = cursor.fetchone()
+    new_id = int(current_highest_id[0]) + 1
+    return new_id
+
+
+# end functions
+
+new_user("Erica Kirk")
